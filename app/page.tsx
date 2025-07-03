1343555317a1bb749c28ac997c8736a4320ec9d9
heyitsdoc <deepaksaini45643@gmail.com>
2025-07-03 07:40:44 +0530

    added sure

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default async function Home() {
         <h1 className="font-semibold text-4xl mb-4 text-slate-950">
           Hi, I’m Doc.
           <span className="block text-slate-500 font-normal text-2xl">
-            I am A builder of things that probably shouldn’t work — but do.
+            I am A builder of things that probably shouldn’t work — but sure do.
           </span>
         </h1>
         <p className="text-slate-700 text-lg md:text-xl leading-normal">
